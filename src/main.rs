deae98cadfd73046ebe20547d709e10b85045bcb
Mariusz Bialonczyk <manio@skyboo.net>
2026-04-05 17:16:23 +0200

    main: fix cmd line arguments
    Command aa-proxy-rs: Short option names must be unique for each argument,
    but '-g' is in use by both 'generate_system_config' and 'generate_hostapd'
    
    credits: @korhojoa

--- src/main.rs
+++ src/main.rs
@@ -72,7 +72,7 @@ struct Args {
     #[clap(short, long)]
     generate_system_config: bool,
     /// Generate hostapd config and exit
-    #[clap(short, long)]
+    #[clap(long)]
     generate_hostapd: bool,
 }
 
